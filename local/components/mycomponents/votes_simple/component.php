9806c91814c17bfe229fdac90fa78f81e3c086c0
vyadev <sl201@inbox.ru>
2024-05-23 15:15:17 +0500

    Update component.php

--- local/components/mycomponents/votes_simple/component.php
+++ local/components/mycomponents/votes_simple/component.php
@@ -2,10 +2,10 @@
 if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 
 use Bitrix\Main\Loader,
-	Bitrix\Iblock;
+  Bitrix\Iblock;
 
 if (!isset($arParams['CACHE_TIME'])) {
-    $arParams['CACHE_TIME'] = 3600;
+  $arParams['CACHE_TIME'] = 3600;
 }
 
 // тип инфоблока
@@ -17,7 +17,7 @@ $arParams['IBLOCK_ID'] = intval($arParams['IBLOCK_ID']);
 // количество элементов на страницу
 $arParams['ELEMENTS_PER_PAGE'] = intval($arParams['ELEMENTS_PER_PAGE']);
 if ($arParams['ELEMENTS_PER_PAGE'] <= 0) {
-    $arParams['ELEMENTS_PER_PAGE'] = 10;
+  $arParams['ELEMENTS_PER_PAGE'] = 10;
 }
 
 // запрещаем сохранение в сессии номера последней страницы
@@ -40,11 +40,6 @@ if ($this->StartResultCache(false, $arNavigation))
   }
 
 
-  // проверяем пришли ли данные из формы
-  if (isset($_POST["id"]) && isset($_POST["rating"])) {
-    $ELEMENT_ID = $_POST["id"];
-    $PROPERTY_VALUE = $_POST["rating"];
-
     // создаём свойство для рейтинга пользователя
     $PROPERTY_CODE = "RATING";
 
@@ -59,45 +54,45 @@ if ($this->StartResultCache(false, $arNavigation))
       $arFields = Array(
         "NAME"          => "Текущий рейтинг",
         "CODE"          => $PROPERTY_CODE,
-        "DEFAULT_VALUE" => 1,
+        "DEFAULT_VALUE" => 0,
         "PROPERTY_TYPE" => 'N',
         "IBLOCK_ID"     => $arParams['IBLOCK_ID'],
       );
       $ibp = new CIBlockProperty();
       $propID = $ibp->Add($arFields);
-      CIBlockElement::SetPropertyValues($ELEMENT_ID, $arParams['IBLOCK_ID'], $PROPERTY_VALUE, $PROPERTY_CODE);
 
       // иначе - заполняем данными из формы
-    } else {
-      if (isset($ELEMENT_ID)) {
+      // проверяем пришли ли данные из формы
+    } else if (isset($_POST["id"]) && isset($_POST["rating"])) {
+        $ELEMENT_ID = $_POST["id"];
+        $PROPERTY_VALUE = $_POST["rating"];
+
         CIBlockElement::SetPropertyValues($ELEMENT_ID, $arParams['IBLOCK_ID'], $PROPERTY_VALUE, $PROPERTY_CODE);
         // CIBlockElement::SetPropertyValueCode($ELEMENT_ID, $PROPERTY_CODE, $PROPERTY_VALUE);
-      }
     }
-  }
 
 
   // выборка данных из инфоблока
-	$obNews = CIBlockElement::GetList(
-		 array("ID" => "desc"),
-		 array(
- 			"IBLOCK_ID" => $arParams['IBLOCK_ID'],
-			"ACTIVE" => "Y"
-		 ),
-		 false,
-		 $arNavParams,
-		 array(
-		 	"ID",
-		 	"PREVIEW_PICTURE",
-		 	"PREVIEW_TEXT",
-		 	"NAME",
-		 	"PROPERTY_RATING",
-		 )
-	);
+  $obNews = CIBlockElement::GetList(
+    array("ID" => "desc"),
+    array(
+      "IBLOCK_ID" => $arParams['IBLOCK_ID'],
+      "ACTIVE" => "Y"
+    ),
+    false,
+    $arNavParams,
+    array(
+      "ID",
+      "PREVIEW_PICTURE",
+      "PREVIEW_TEXT",
+      "NAME",
+      "PROPERTY_RATING",
+    )
+  );
 
-	while ($resNews = $obNews->Fetch()) {
-		$arResult["ELEMENTS"][] = $resNews;
-	}
+  while ($resNews = $obNews->Fetch()) {
+    $arResult["ELEMENTS"][] = $resNews;
+  }
 
 
   // кастомная пагинация system.pagenavigation с шаблоном show_more_v2
@@ -111,20 +106,20 @@ if ($this->StartResultCache(false, $arNavigation))
     $this
   );
 
-	// регистрируем тегированный кэш для вывода свойств
+  // регистрируем тегированный кэш для вывода свойств
   if (defined("BX_COMP_MANAGED_CACHE") && is_object($GLOBALS['CACHE_MANAGER'])){
     $GLOBALS['CACHE_MANAGER']->RegisterTag('votes_simple');
   }
 
-	$this->SetResultCacheKeys(array(
-	 	/*"ID",
-	 	"PREVIEW_PICTURE",
-	 	"PREVIEW_TEXT",
-	 	"NAME",
-	 	"PROPERTY_RATING",*/
-	 ));
+  $this->SetResultCacheKeys(array(
+    /*"ID",
+    "PREVIEW_PICTURE",
+    "PREVIEW_TEXT",
+    "NAME",
+    "PROPERTY_RATING",*/
+  ));
 
-	$this->includeComponentTemplate();
+  $this->includeComponentTemplate();
 }
 
 // сбрасываем тегированный кэш

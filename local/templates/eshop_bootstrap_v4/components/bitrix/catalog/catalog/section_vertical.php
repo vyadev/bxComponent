14aaa98349ea9f8ad5424bd0466775b5d8c1cda1
vyadev <sl201@inbox.ru>
2024-05-29 15:20:38 +0500

    Update section_vertical.php

--- local/templates/eshop_bootstrap_v4/components/bitrix/catalog/catalog/section_vertical.php
+++ local/templates/eshop_bootstrap_v4/components/bitrix/catalog/catalog/section_vertical.php
@@ -448,14 +448,11 @@ else
 							<? $APPLICATION->IncludeComponent("bitrix:catalog.section", "bootstrap_v4", array(
 									"IBLOCK_TYPE" => $arParams["IBLOCK_TYPE"],
 									"IBLOCK_ID" => $arParams["IBLOCK_ID"],
-
-                  "ELEMENT_SORT_FIELD" => "PROPERTY_CART_COUNT",
-                  "ELEMENT_SORT_ORDER" => "ASC",
-
-//									"ELEMENT_SORT_FIELD" => $arParams["ELEMENT_SORT_FIELD"],
-//									"ELEMENT_SORT_ORDER" => $arParams["ELEMENT_SORT_ORDER"],
-//									"ELEMENT_SORT_FIELD2" => $arParams["ELEMENT_SORT_FIELD2"],
-//									"ELEMENT_SORT_ORDER2" => $arParams["ELEMENT_SORT_ORDER2"],
+	
+									"ELEMENT_SORT_FIELD" => $arParams["ELEMENT_SORT_FIELD"],
+									"ELEMENT_SORT_ORDER" => $arParams["ELEMENT_SORT_ORDER"],
+									"ELEMENT_SORT_FIELD2" => $arParams["ELEMENT_SORT_FIELD2"],
+									"ELEMENT_SORT_ORDER2" => $arParams["ELEMENT_SORT_ORDER2"],
 									"PROPERTY_CODE" => (isset($arParams["LIST_PROPERTY_CODE"]) ? $arParams["LIST_PROPERTY_CODE"] : []),
 									"PROPERTY_CODE_MOBILE" => $arParams["LIST_PROPERTY_CODE_MOBILE"],
 									"INCLUDE_SUBSECTIONS" => $arParams["INCLUDE_SUBSECTIONS"],
